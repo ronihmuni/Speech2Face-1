b5676a04132e0967c54d9987061f5b437ba6b938
ronihmuni <ronihamri@mail.tau.ac.il>
2022-07-17 23:08:30 +0300

    wav decode

--- spectrogram_loader.py
+++ spectrogram_loader.py
@@ -2,6 +2,7 @@
 # and return a tensor compatible for the network
 import tensorflow as tf
 
+
 class SpectrogramLoader():
     def __init__(self, filename):
         self.filename = filename
@@ -9,3 +10,4 @@ class SpectrogramLoader():
     def getTensor(self):
         raw_audio = tf.io.read_file(self.filename)
         waveform = tf.audio.decode_wav(raw_audio)
+        return waveform
